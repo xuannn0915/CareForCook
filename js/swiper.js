e791ae1fcfdffe5ec9ed7dd739b0ab4b15a71547
xuan <xuannn0915@gmail.com>
2023-08-17 01:07:54 +0800

    swiper update

--- js/swiper.js
+++ js/swiper.js
@@ -7,25 +7,17 @@ var swiper = new Swiper(".mySwiper", {
     delay: 2000,
     disableOnInteraction: true,
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
     // Responsive breakpoints
     breakpoints: {
       // when window width is >= 320px
       320: {
-        slidesPerView: 2,
+        slidesPerView: 1.5,
         spaceBetween: 20
       },
       // when window width is >= 480px
       // when window width is >= 992px
       992: {
-        slidesPerView: 3.25,
+        slidesPerView: 2.5,
         spaceBetween: 30
       }
     }
