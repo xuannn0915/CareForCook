9d39ccdd550c291f06ba55137632ee56ff189f99
xuan <xuannn0915@gmail.com>
2023-08-18 01:36:53 +0800

    service complete

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,5 @@
-// Initialize Swiper 
-var swiper = new Swiper(".mySwiper", {
+// banner swiper
+var bannerSwiper = new Swiper(".bannerSwiper", {
   spaceBetween: 24,
   centeredSlides: true,
   loop: true,
@@ -7,18 +7,42 @@ var swiper = new Swiper(".mySwiper", {
     delay: 2000,
     disableOnInteraction: true,
   },
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1.5,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      // when window width is >= 992px
-      992: {
-        slidesPerView: 2.5,
-        spaceBetween: 30
-      }
-    }
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// service swiper
+var serviceSwiper = new Swiper(".serviceSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 1.1,
+      spaceBetween: 8,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.1,
+      spaceBetween: 16,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
 });
