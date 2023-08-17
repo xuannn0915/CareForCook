86c0d01151ee1450e37d44d2e07892fdd0c6ba3f
xuan <xuannn0915@gmail.com>
2023-08-18 00:32:57 +0800

    service tabs finish swiper in progress

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,14 @@
 // tabs
-$( function() {
-  $( "#tabs" ).tabs();
-} );
+$(function () {
+  $("#tabs").tabs();
+});
+
+
+$(function () {
+  $(".nav-item").click(function () {
+    // remove classes from all
+    $(".nav-link").removeClass("active");
+    // add class to the one we clicked
+    $(this).children('a').addClass("active");
+  });
+});
