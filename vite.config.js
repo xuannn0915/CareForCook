0ef03e93b3c8380ec3f0762fc7d4f397a9819a79
xuan <xuannn0915@gmail.com>
2023-08-19 14:21:33 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/CareForCook/',
+  base: '/Care4Cook/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
