531208382fbafe4cd82158665a981e26f6f61668
xuan <xuannn0915@gmail.com>
2023-08-20 16:19:58 +0800

    login page complete

--- js/tabs.js
+++ js/tabs.js
@@ -14,6 +14,13 @@ $(function () {
 });
 
 $(function () {
+    // sign up an account
+    $(".signUp").click(function (event) {
+      event.preventDefault();
+      $(".login").removeClass('active').addClass("disabled");
+      $(".register").removeClass("disabled").addClass('active');
+    });
+
   // to forget password page
   $(".forget").click(function (event) {
     event.preventDefault();
@@ -22,9 +29,12 @@ $(function () {
   });
 
   // return to login page
-  $(".retun_login").click(function (event) {
+  $(".return_login").click(function (event) {
     event.preventDefault();
     $(".forgetPassword").removeClass('active').addClass("disabled");
+    $(".register").removeClass('active').addClass("disabled");
     $(".login").removeClass("disabled").addClass('active');
   });
+
+
 });
