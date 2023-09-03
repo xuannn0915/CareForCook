73d9aed3eb16d98359e0a603efb930d699c4b5a3
xuan <xuannn0915@gmail.com>
2023-09-03 17:22:27 +0800

    shopping bag complete

--- js/tabs.js
+++ js/tabs.js
@@ -23,3 +23,31 @@ $(function () {
     $(this).children('a').addClass("active");
   });
 });
+
+$(function () {
+  $(".wizard-tab").click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({ // 有些瀏覽器只支援html，有些只支援body 所以兩個都寫進去
+      scrollTop: 0,
+    }, 0);
+    $('.wizard-tab:nth-child(odd)').css('display','block');
+    $(this).css('display','none')
+  });
+  $(".tab-3").click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({ // 有些瀏覽器只支援html，有些只支援body 所以兩個都寫進去
+      scrollTop: 0,
+    }, 0);
+    $('.wizard-tab:nth-child(even)').css('display','block');
+    $('.wizard-tab:nth-child(odd)').css('display','none')
+  });
+  $(".tab-1").click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({ // 有些瀏覽器只支援html，有些只支援body 所以兩個都寫進去
+      scrollTop: 0,
+    }, 0);
+    $('.orderSummary').css('display','none');
+    $('.receiveOrder').css('display','block');
+    $('.wizard-tab:nth-child(odd)').css('display','none')
+  });
+});
